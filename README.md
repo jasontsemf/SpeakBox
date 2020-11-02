76d6150c25755433b8dc9229900e8a9468d8072c
Jason <jasontsemf@gmail.com>
2020-11-01 19:27:38 -0500

    readme and emoji

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 # Speak Box
 
 [Live demo on glitch.com](https://speakbox.glitch.me/)
+![speakbox](doc/screencap.png)
 
 ## Intro
 
-Speak Box is a beat box sequencer built with the speech synthesis functionality from the <a href="https://developer.mozilla.org/en-US/docs/Web/API/Web_Speech_API">Web Search API</a>, altogether with <a href="https://github.com/SortableJS/sortablejs">SortableJS</a>, and <a href="https://materializecss.com/">Materialize</a>.
+Speak Box is a beat box sequencer built with the speech synthesis functionality from the <a href="https://developer.mozilla.org/en-US/docs/Web/API/Web_Speech_API">Web Speech API</a>, altogether with <a href="https://github.com/SortableJS/sortablejs">SortableJS</a>, and <a href="https://materializecss.com/">Materialize</a>.
+
+More information on my [Wordpress Blog](https://jason1996429.wordpress.com/2020/11/01/hello-computer-wk-1-speak-box/)
--- js/script.js
+++ js/script.js
@@ -41,7 +41,6 @@ const speak = text => {
     let utterThis = new SpeechSynthesisUtterance(text);
     utterThis.rate = map(speed.value, 1, 100, 0
         , 2);
-    console.log(utterThis.rate);
     synth.speak(utterThis);
 }
 
@@ -50,31 +49,14 @@ const map = (num, in_min, in_max, out_min, out_max) => {
 }
 
 document.querySelector("#play").onclick = () => {
-    // let textInput = document.querySelector("#text-input").value;
-    // let phrase = "";
-
-    // let split = textInput.split("");
-
-    // split.forEach(letter =>{
-    //     // console.log(letter);
-    //     // for (i = 0; i < 5; i++){
-    //         phrase += letter;
-    //     // }
-    // })
-    // speak(phrase);
-    // console.log(phrase);
-
-    var node, list, arrValue;
-    // list = [];
+    var node;
     let phrase = "";
     let fullPhrase = "";
     let msgDom = document.querySelector(".msg");
 
     for (node = document.getElementById('list').firstChild; node; node = node.nextSibling) {
         if (node.nodeType == 1 && node.tagName == 'LI') {
-            // list.push(node.innerHTML);
             phrase += node.getAttribute('alt') + " ";
-            // console.log(node.getAttribute('alt'));
         }
     }
     for (x = 0; x < rep.value; x++){
@@ -85,7 +67,6 @@ document.querySelector("#play").onclick = () => {
 }
 
 
-
 document.querySelector("#clear").onclick = () => {
     document.querySelector("#list").innerHTML = "";
     document.querySelector(".msg").innerHTML = "";
@@ -94,19 +75,3 @@ document.querySelector("#clear").onclick = () => {
 document.querySelector("#stop").onclick = () => {
     synth.cancel();
 }
-
-// let speed = document.getElementById("speed");
-// let outputSpeedSpeed = document.getElementById("outputSpeed");
-
-// let rep = document.getElementById("rep");
-// let outputRep = document.getElementById("outputRep");
-
-// outputSpeed.innerHTML = speed.value + "% speed" ;
-// outputRep.innerHTML = "repeat " + rep.value + " times";
-
-// speed.oninput = function() {
-//     outputSpeed.innerHTML = this.value + "% speed";
-// }
-// rep.oninput = function() {
-//     outputRep.innerHTML = "repeat " + this.value + " times";
-// }
